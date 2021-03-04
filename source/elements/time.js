03be5204e7bdc7bc45dd12c6f097ade15f077ac1
Daniel Scroggins <61557261+dscrog@users.noreply.github.com>
2021-03-04 10:07:46 -0500

    merge repositories

--- source/elements/time.js
+++ source/elements/time.js
@@ -1,9 +1,5 @@
-import html from '../templates/time.js';
-
 export class Time extends Quantum {
     constructor() {
         super();
     }
 }
-
-Time.define('quantum-time', html);
--- source/main.js
+++ source/main.js
@@ -1,1 +1,4 @@
-import './elements/time.js';
+import { Time } from './elements/time.js';
+import time from './templates/time.js';
+
+Time.define('quantum-time', time);

0b913518d8e1db0afb2d28501aa6f6496204ca53
Daniel Scroggins <61557261+dscrog@users.noreply.github.com>
2021-07-12 15:29:03 -0400

    update build

--- source/elements/time.js
+++ source/elements/time.js
@@ -1,5 +1,9 @@
+import time from '../templates/time.js';
+
 export class Time extends Quantum {
     constructor() {
         super();
     }
 }
+
+Time.define('quantum-time', time);
--- source/main.js
+++
@@ -1,4 +0,0 @@
-import { Time } from './elements/time.js';
-import time from './templates/time.js';
-
-Time.define('quantum-time', time);
